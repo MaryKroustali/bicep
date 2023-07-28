5a848599d4fac9a581bc16291a3c5407d666b0f3
Mary Kroustali <71144000+MaryKroustali@users.noreply.github.com>
2023-07-28 15:42:43 +0300

    Update setup_jumpbox.sh

--- setup_jumpbox.sh
+++ setup_jumpbox.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-echo "Install kubectl ..."
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl lsb-release gnupg-agent software-properties-common mysql-client
+curl -fsSL https://get.docker.com -o get-docker.sh && sudo chmod +x get-docker.sh && ./get-docker.sh
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-get update -y
+sudo apt-get install azure-cli -y
 sudo az aks install-cli
-sudo chown -R azureuser /var/run/docker
-echo "Install helm ..." >> $log
-curl -o helm.tar.gz https://get.helm.sh/helm-v3.8.0-linux-amd64.tar.gz
-tar zxvf helm.tar.gz
-sudo mv linux-amd64/helm /usr/local/bin/helm
-rm -rf linux-amd64
-rm -f helm.tar.gz
-echo "login with VMSS Manage Identity ..."
-az login --identity
-echo "Connect to Cluster"
-az aks get-credentials --name aks-cnsp-dev-weu --resource-group rg-cnsp-dev-weu-mkrou --overwrite-existing 
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+sudo az aks get-credentials -g rg-cnsp-dev-weu-diama -n aks-cnsp-dev-weu
