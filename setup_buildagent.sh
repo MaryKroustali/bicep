39e6996f62503f9a89ef4537c8367fd8039f2aa0
Mary Kroustali <71144000+MaryKroustali@users.noreply.github.com>
2024-01-31 15:47:49 +0200

    Update setup_buildagent.sh

--- setup_buildagent.sh
+++ setup_buildagent.sh
@@ -44,22 +44,21 @@ sudo apt-get install azure-cli -y
 
 # download azdo agent
 echo "Install Azure DevOps agent ..." >> $log
-sudo mkdir -p /opt/azdo && cd /opt/azdo
-cd /opt/azdo
-sudo curl -o azdoagent.tar.gz https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-linux-x64-3.225.0.tar.gz
-sudo tar xzvf azdoagent.tar.gz
-sudo rm -f azdoagent.tar.gz
+sudo mkdir myagent && cd myagent
+sudo curl https://vstsagentpackage.azureedge.net/agent/3.232.3/vsts-agent-linux-x64-3.232.3.tar.gz
+sudo tar xzvf vsts-agent-linux-x64-3.232.3.tar.gz
+sudo rm -f vsts-agent-linux-x64-3.232.3.tar.gz
 
 # configure as azdouser
 echo "Configure Azure DevOps agent ..." >> $log
-sudo chown -R $agentuser /opt/azdo
-sudo chmod -R 755 /opt/azdo
-runuser -l $agentuser -c "/opt/azdo/config.sh --unattended --url $azdourl --auth pat --token $pat --pool $pool --acceptTeeEula"
+sudo chown -R $agentuser myagent
+sudo chmod -R 755 myagent
+runuser -l $agentuser -c "myagent/config.sh --unattended --url $azdourl --auth pat --token $pat --pool $pool --acceptTeeEula"
 
 # install and start the service
 echo "Configure Azure DevOps agent to run as a service ..." >> $log
-sudo /opt/azdo/svc.sh install
-sudo /opt/azdo/svc.sh start
+sudo myagent/svc.sh install
+sudo myagent/svc.sh start
 
 # give permissions to run pipeline
 echo "Configure Azure DevOps agent to run Docker scripts ..." >> $log
