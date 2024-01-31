0f58c3f785802b2eda98ecc211eff2936af57cc1
Mary Kroustali <71144000+MaryKroustali@users.noreply.github.com>
2024-01-31 17:12:47 +0200

    Update setup_buildagent.sh

--- setup_buildagent.sh
+++ setup_buildagent.sh
@@ -51,15 +51,9 @@ cd /opt/azdo
 sudo curl -o azdoagent.tar.gz https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-linux-x64-3.225.0.tar.gz
 sudo tar xzvf azdoagent.tar.gz
 sudo rm -f azdoagent.tar.gz
-
-echo "Configure Azure DevOps agent ..." >> $log
 sudo chown -R $agentuser /opt/azdo
 sudo chmod -R 755 /opt/azdo
 runuser -l $agentuser -c "/opt/azdo/config.sh --unattended --url $azdourl --auth pat --token $pat --pool $pool --acceptTeeEula"
-
-echo "Configure Azure DevOps agent to run as a service ..." >> $log
 sudo /opt/azdo/svc.sh install
 sudo /opt/azdo/svc.sh start
-
-# give permissions to run pipeline
 sudo chown -R $agentuser /var/run/docker
